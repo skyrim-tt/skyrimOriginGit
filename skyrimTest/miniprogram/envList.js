1923d4274d6133b3ef95f6d2b41d57cb3ddfea40
skyrim-tt <tt@mubio-tech.com>
2021-09-04 18:12:54 +0800

    AllFiles

---
+++ skyrimTest/miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-9gx01yhh58357807","alias":"cloud1"}]
+const isMac = true
+module.exports = {
+    envList,
+    isMac
+}
---
+++ skyrimTest/uploadCloudFunction.sh
@@ -0,0 +1,1 @@
+"/Applications/wechatwebdevtools.app/Contents/MacOS/cli" cloud functions deploy --e cloud1-9gx01yhh58357807 --n quickstartFunctions --r --project "/Users/skyrim/skyrimGit/skyrimTest" --report
